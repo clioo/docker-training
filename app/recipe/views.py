060de517ced8de6f54b7107d73953cd73a2b85a0
clioo <jesus_acosta1996@hotmail.com>
2020-04-20 15:51:55 -0600

    linter fixed

--- app/recipe/views.py
+++ app/recipe/views.py
@@ -15,15 +15,19 @@ class BaseRecipeAttrViewSet(viewsets.GenericViewSet,
     def get_queryset(self):
         """Return objects for the current authenticated user only"""
         return self.queryset.filter(user=self.request.user).order_by('-name')
+
     def perform_create(self, serializer):
         """Create new object"""
         serializer.save(user=self.request.user)
 
+
 class TagViewSet(BaseRecipeAttrViewSet):
     """Manage tags in the database"""
     queryset = Tag.objects.all()
+    serializer_class = serializers.TagSerializer
 
 
 class IngredientViewSet(BaseRecipeAttrViewSet):
     """Manage ingredients in the database"""
     queryset = Ingredient.objects.all()
+    serializer_class = serializers.IngredientSerializer
