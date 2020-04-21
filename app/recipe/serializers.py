e1f03e00947acf8ded0c680c8fe931eaa8978e97
clioo <jesus_acosta1996@hotmail.com>
2020-04-20 22:27:35 -0600

    recipedetailviewset created

--- app/recipe/serializers.py
+++ app/recipe/serializers.py
@@ -36,3 +36,9 @@ class RecipeSerializer(serializers.ModelSerializer):
         fields = ('id', 'title', 'ingredients', 'time_minutes', 'tags',
                   'price', 'link',)
         read_only_fields = ('id',)
+
+
+class RecipeDetailSerializer(RecipeSerializer):
+    """Serialize a recipe detail"""
+    ingredients = IngredientSerializer(many=True, read_only=True)
+    tags = TagSerializer(many=True, read_only=True)
